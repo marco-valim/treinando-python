23caad3f86c26349f2cc1f809ea17d934034f09a
marco-valim <marco.antvs@gmail.com>
2025-06-04 17:02:21 -0400

    exercicio count

---
+++ estrutura_de_dados/count.py
@@ -0,0 +1,4 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 5, 8]
+valor = int(input(f'\nInforme o número que deseja contar quantas vezes repete: '))
+
+print(f'O número {valor} aparece {lista.count(valor)} vezes na lista.\n')
