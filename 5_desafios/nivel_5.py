a7f4773f8a2ca98942e81ae4d0fe380d139e534c
marco-valim <marco.antvs@gmail.com>
2025-06-04 10:27:55 -0400

    adicionando o nivel 5 do desafio

--- 5_desafios/nivel_5.py
+++ 5_desafios/nivel_5.py
@@ -1,2 +1,24 @@
 # Validador de senha forte
 
+import pwinput
+
+senha = str(pwinput.pwinput(f"""\nSua nova senha deve atender os seguinte requisitos:
+    - Pelo menos 8 caracteres de comprimento.
+    - Conter pelo menos uma letra maiúscula.
+    - Conter pelo menos uma letra minúscula.
+    - Conter pelo menos um número.
+    - Conter pelo menos um caractere especial (!, @, #, $, %, ^, &, *).
+                                                               
+Informe a senha desejada: """, mask="*"))
+
+INVALIDO = "Senha inválida, critérios não atendidos!"
+VALIDO = "Senha forte, criada com sucesso!"
+CARAC_ESPECIAL = ["!, @, #, $, %, &, *"]
+
+if (not any(caractere.isupper() for caractere in senha)) or \
+    (not any(caractere.islower() for caractere in senha)) or \
+    (not any(caractere.isdigit() for caractere in senha)) or \
+    (any(caractere in CARAC_ESPECIAL for caractere in senha)):
+    print(INVALIDO,'\n')
+else:
+    print(VALIDO,'\n')
