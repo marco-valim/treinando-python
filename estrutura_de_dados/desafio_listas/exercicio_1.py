6ea059209d798166058d4bf283a0d1ccd6f17599
marco-valim <marco.antvs@gmail.com>
2025-06-04 18:01:09 -0400

    desafios criados

---
+++ estrutura_de_dados/desafio_listas/exercicio_1.py
@@ -0,0 +1,17 @@
+# Crie uma lista chamada lista_compras com os seguintes itens: "maçã", "banana", "cenoura".
+lista_compras = ["maçã", "banana", "cenoura"]
+
+# Adicione "laranja" ao final da lista.
+lista_compras.insert(3,"laranja")
+
+# Adicione "uva" na segunda posição da lista.
+lista_compras.insert(1,"uva")
+
+# Remova "cenoura" da lista.
+lista_compras.pop(3)
+
+# Imprima o item na primeira posição da lista.
+print(lista_compras[0])
+
+# Imprima a lista final.
+print(lista_compras)
