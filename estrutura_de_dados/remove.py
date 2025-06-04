27d0a173c6d9c4649eaafe586efb16a7a6bf7fb8
marco-valim <marco.antvs@gmail.com>
2025-06-04 14:59:10 -0400

    adicionando o arquivo remove.py

---
+++ estrutura_de_dados/remove.py
@@ -0,0 +1,8 @@
+lista = [1, 2, 5, 3, 4, 5, 6, 7, 8, 9, 10]
+
+posicao = int(input(f'''\nLista: {lista}
+Infome a posição na lista para remover o valor: '''))
+
+
+lista.remove(posicao)
+print(f'\nLista atualizada {lista}\n')
