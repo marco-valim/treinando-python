455d1fcccf7fb4259562c161ce45fb6d16f204c7
marco-valim <marco.antvs@gmail.com>
2025-06-04 15:13:27 -0400

    exercicio sort.py

---
+++ estrutura_de_dados/sort.py
@@ -0,0 +1,8 @@
+lista = [51, 82, 97, 41, 22, 35, 12, 87, 63, 32, 28]
+print(f'\nLista desordenada {lista}\n')
+
+lista.sort()
+print(f'Lista em ordem crescente {lista}\n')
+
+lista.sort(reverse=True)
+print(f'Lista em ordem decrescente {lista}\n')
