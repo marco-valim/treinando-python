645393fca9b232e7f3ab37dbe2c70f4cb3740507
marco-valim <marco.antvs@gmail.com>
2025-06-04 18:01:51 -0400

    atualização index.py

--- estrutura_de_dados/index.py
+++ estrutura_de_dados/index.py
@@ -1,3 +1,3 @@
 lista = [5, 6, 7, 4, 2, 8 , 9, 1, 3]
 
-print(f'Você descobriu o valor: {lista.index(int(input(f'\nInforme a posição da lista que deseja saber o valor: ')))}\n')
+print(f'O valor está na posição: {lista.index(int(input(f'\nInforme o valor que deseja descobrir a posição na lista: ')))}\n')
