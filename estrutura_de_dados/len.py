b3a85b8fe7dd13ac53d3cda51735bff3b4d9d95a
marco-valim <marco.antvs@gmail.com>
2025-06-05 10:11:33 -0400

    melhorando a leitura

--- estrutura_de_dados/len.py
+++ estrutura_de_dados/len.py
@@ -1,6 +1,6 @@
-lista = []
+lista = ["4","8","6"]
 
-while True:
-    lista.append(int(input(f'\nInsira valores inteiros positivos na lista: ')))
-    print(f'''\nLista atualizada: {lista}
+#while True:
+   # lista.append(int(input(f'\nInsira valores inteiros positivos na lista: ')))
+print(f'''\nLista atualizada: {lista}
 Quantidade de valores na lista: {len(lista)}''')
