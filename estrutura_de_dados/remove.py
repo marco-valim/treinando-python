977505ac39f9fd5c1135f65a4e8b38cc838e5c77
marco-valim <marco.antvs@gmail.com>
2025-06-07 13:08:20 -0400

    atualizando exercicio_3.py e remove.py

--- estrutura_de_dados/remove.py
+++ estrutura_de_dados/remove.py
@@ -1,7 +1,7 @@
 lista = [1, 2, 5, 3, 4, 5, 6, 7, 8, 9, 10]
 
 posicao = int(input(f'''\nLista: {lista}
-Infome o valor na lista para remover o valor: '''))
+Infome o valor na lista para remove-lo: '''))
 
 
 lista.remove(posicao)
