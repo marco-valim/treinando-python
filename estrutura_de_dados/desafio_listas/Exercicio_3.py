52d12b39931bf9640e5c1a0c7fc6893a9435c66e
marco-valim <marco.antvs@gmail.com>
2025-06-07 13:10:18 -0400

    atualizando exercicio_3.py

--- estrutura_de_dados/desafio_listas/Exercicio_3.py
+++ estrutura_de_dados/desafio_listas/Exercicio_3.py
@@ -1,20 +1,35 @@
 tarefas = []
+tarefas_concluidas = []
+menu = ''
 
-menu = str(input(f'''\n-------------MENU-------------
+while menu != '5':
+    menu = str(input(f'''\n-------------MENU-------------
 1 - Adicionar nova tarefa
-2 - Vizualizar todas tarefas
-3 - Concluir tarefa
-4 - Sair do programa
-                 
+2 - Vizualizar tarefas
+3 - Vizualizar tarefas concluídas
+4 - Concluir tarefa
+5 - Sair do programa
+                
 Escolha as opções do menu: '''))
 
 
-if menu == '1':
-    tarefas.append(str(input(f'Informa a tarefa a ser adicionada: ')))
+    if menu == '1':
+        tarefas.append(str(input(f'Informa a tarefa a ser adicionada: ')))
+        continue
 
+    elif menu == '2':
+        print(f'Lista de tarefas: {tarefas}')
+        continue
 
+    elif menu == '3':
+        print(f'Lista de tarefas concluídas: {tarefas_concluidas}')
+        continue
 
-
-
-
-print(tarefas)
+    elif menu == '4':
+        concluida =  str(input(f'Informe a tarefa que deseja concluir: '))
+        tarefas_concluidas.append(concluida)
+        tarefas.remove(concluida)
+        continue
+        
+    else:
+        print('Programa encerrado\n')
