03bcd263c4e4127becf574ae1d4550d88ba787f4
marco-valim <marco.antvs@gmail.com>
2025-06-10 15:47:29 -0400

    atualização treino 4

--- estrutura_de_dados/desafio_listas/treino_4.py
+++ estrutura_de_dados/desafio_listas/treino_4.py
@@ -4,15 +4,28 @@ alunos = [
     {"nome": "Marta", "idade": 25, "nota": 8.7}, 
     {"nome": "Jonas", "idade": 28, "nota": 5.5}
 ]
+lista_aprovados = []
+soma_notas = 0
 
 # Conta a quanditade de alunos
-soma_notas = 0
 quantidade_alunos = len(alunos)
 
 # Faz a soma das notas
-for alunos in alunos:
-    soma_notas += alunos["nota"]
+for aluno in alunos:
+    soma_notas += aluno["nota"]
 
 # Calcula a média das notas
 media = soma_notas / quantidade_alunos
 print(f'\nA media é: {media:.2f}\n')
+
+# Lista com apenas os alunos que têm nota >= 7
+for aluno in alunos:
+    if aluno["nota"] >= 7:
+        lista_aprovados.append(aluno["nome"])  
+print(f'Alunos aprovados: {lista_aprovados}\n')
+
+# Ordene a lista alunos da maior para a menor nota.
+alunos.sort(key=lambda aluno: aluno["nota"], reverse=True)
+print('Alunos ordenados pela maior nota: ')
+for aluno in alunos:
+    print(f'{aluno["nome"]} - {aluno["nota"]}')
